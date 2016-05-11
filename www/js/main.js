db69be6ee4a581fb22ea6c76fd6a5ff7c0f9a343
rath-prak <rathanap@gmail.com>
2016-05-11 19:19:49 +1200

    fix js file

--- www/js/main.js
+++ www/js/main.js
@@ -5,7 +5,7 @@ var request = require('superagent')
 $(document).ready(function() {
  console.log("document ready")
 
-// $('#pagepiling').pagepiling({direction: 'horizontal', loopBottom: 'true'});
+
 
   //Tells jquery to wait until dom has loaded before loading jquery
   $("#button").click(searchActor)
@@ -19,6 +19,7 @@ function searchActor() {
     getMovieByActor(rawInput, function(err, data){
       if (data.body.results.length > 0) {
           var movies = data.body.results[0].known_for
+          var movieId = data.body.results[0].known_for.id // get movie id
           renderMovieResults(movies)
           renderActor(data.body.results[0])
         if(data.body.results[1].adult == true){
@@ -41,10 +42,39 @@ function searchActor() {
     //create outer div with an animation
     for (var i = 0; i < movies.length; i++) {
       var movieResult = document.createElement('div')
+      // var moviePopup = document.createElement('div')
       movieResult.className = 'posters'
-      // movieResult.innerHTML = "<h3>" + movies[i].title + "</h3><br>" + "<img src='https://image.tmdb.org/t/p/w185/" + movies[i].poster_path +"'><br>"
-      movieResult.innerHTML = "<img src='https://image.tmdb.org/t/p/w185/" + movies[i].poster_path +"'><br>"
+      // movieResult.innerHTML = "<img src='https://image.tmdb.org/t/p/w396/" + movies[i].poster_path +"'><br>" + "<h5>" + movies[i].title + "</h5><br>" + "<p class='overviewText'>" + movies[i].overview + "</p>"
+      movieResult.innerHTML = "<div class='movieResult'><img src='https://image.tmdb.org/t/p/w396/" + movies[i].poster_path +"'><br>" + "<h5>" + movies[i].title + "</h5><br>" + "<p class='overviewText'>" + movies[i].overview + "</p></div>"
+
+      
+
+      // movieResult.innerHTML = "<img src='https://image.tmdb.org/t/p/w185/" + movies[i].poster_path +"'><br>"
       document.getElementById('results').appendChild(movieResult)
+
+              // $( ".posters" ).click(function() {
+              //     $(this).toggleClass('overview')
+              //     // alert('this click is working')
+
+
+              $( ".posters" ).click(function() {
+                 // $(this).toggleClass("overviewTextShow")
+                  // $(this).closest(".overviewText").toggleClass('overview')
+                  // alert('this click is working')
+                  window.location.href = "https://www.themoviedb.org/"
+        });
+
+        //       $( ".posters" ).click(function() {
+        //           $(this).on({
+        //             popupbeforeposition: function() {
+        //                 var maxHeight = $( window ).height() - 60 + "px";
+        //                 $( ".photopopup img" ).css( "max-height", maxHeight );
+        //           }
+        //       });
+
+        // });
+
+
     }
   }
 
@@ -52,7 +82,6 @@ function searchActor() {
     // document.getElementById('face').className = "animated fadeIn"
     // document.getElementById('face').innerHTML = "<h3>" + actor.name + "</h3><br>" + "<img src='http://image.tmdb.org/t/p/w185/" + actor.profile_path +"'><br>"
     document.getElementById('face').innerHTML = "<h3>" + actor.name + "</h3><br>" + "<img src='http://image.tmdb.org/t/p/w780/" + actor.known_for[0].backdrop_path +"'><br>"
-    // document.getElementById('face').innerHTML = "<h3>" + actor.name + "</h3><br>" + "<img src='http://image.tmdb.org/t/p/w780/" + actor.backdrop_path +"'><br>"
   }
 
 
@@ -67,7 +96,9 @@ function searchActor() {
         }
         callback(null, res)
     })
-
   }
-})
+
+
+
+}) //end of document ready
 
